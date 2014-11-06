cd65693978514d4432a226efc52a34252f1aef80
Dennis Hellmich <dennish@gmx.de>
2014-11-06 22:40:54 +0100

    removed second mybudget direcory

--- mybudget/wsgi.py
+++ mybudget/wsgi.py
@@ -1,16 +1,15 @@
 """
-WSGI config for gettingstarted project.
+WSGI config for mybudget project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mybudget.mybudget.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mybudget.settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
