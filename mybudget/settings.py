f9077ec75363ce22947bad70784d58648ab952c7
Dennis Hellmich <dennish@gmx.de>
2014-11-07 00:04:03 +0100

    settings

--- mybudget/settings.py
+++ mybudget/settings.py
@@ -91,8 +91,8 @@ USE_TZ = True
 #STATICFILES_DIRS = (
 #    os.path.join(BASE_DIR, "static"),
 #)
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR + "/../static/"
+#STATIC_URL = '/static/'
+#STATIC_ROOT = BASE_DIR + "/static/"
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
