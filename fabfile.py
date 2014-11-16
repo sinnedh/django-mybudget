481f5017d994e2e8bc2d464b1686611331413be5
Dennis Hellmich <dennish@gmx.de>
2014-11-16 22:53:41 +0100

    added simple fabfile

---
+++ fabfile.py
@@ -0,0 +1,34 @@
+from __future__ import with_statement
+from fabric.api import local
+
+
+def clean_pyc(settings='mybudget.settings.development'):
+    local('python manage.py clean_pyc --settings={}'.format(settings))
+
+
+def collectstatic(settings='mybudget.settings.development'):
+    clean_pyc(settings)
+    local('python manage.py collectstatic --settings={}'.format(settings))
+
+
+def runserver(settings='mybudget.settings.development'):
+    clean_pyc(settings)
+    local('python manage.py runserver --settings={}'.format(settings))
+
+
+def migrate(settings='mybudget.settings.development'):
+    clean_pyc(settings)
+    local('python manage.py makemigrations --settings={}'.format(settings))
+    local('python manage.py migrate --settings={}'.format(settings))
+
+
+def test(settings='mybudget.settings.development'):
+    clean_pyc(settings)
+    local('python manage.py test --failfast --settings={}'.format(settings))
+
+
+def deploy(settings='mybudget.settings.production'):
+    local('git push heroku master')
+    local('heroku run python manage.py collectstatic --settings={}'.format(settings))
+#    local('heroku run python manage.py makemigrations --settings={}'.format(settings))
+    local('heroku run python manage.py migrate --settings={}'.format(settings))
