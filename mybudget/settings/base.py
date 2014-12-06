5935b50ad7d0b344057030e39f81447ab940c539
Dennis Hellmich <dennish@gmx.de>
2014-12-06 13:57:29 +0100

    Updated static settings

--- mybudget/settings/base.py
+++ mybudget/settings/base.py
@@ -83,7 +83,7 @@ USE_TZ = True
 STATICFILES_DIRS = (
 #    os.path.join(BASE_DIR, "static"),
 )
-STATIC_ROOT = BASE_DIR + "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 #STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
