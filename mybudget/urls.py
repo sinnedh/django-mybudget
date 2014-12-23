9a05c609825b94beb899690f644b941bd29883eb
Dennis Hellmich <dennish@gmx.de>
2014-12-23 13:55:06 +0100

    simplified url import of views

--- mybudget/urls.py
+++ mybudget/urls.py
@@ -2,16 +2,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from mybudget.views import DashboardView
-from mybudget.views import (ExpenseCreateView,
-                            ExpenseUpdateView,
-                            ExpenseDeleteView,
-                            FilteredExpenseListView,
-                            LatestExpenseListView,)
-from mybudget.views import (CategoryListView,
-                            CategoryCreateView,
-                            CategoryUpdateView,
-                            CategoryDeleteView,)
+from mybudget import views
+
 
 urlpatterns = patterns(
     '',
@@ -24,18 +16,18 @@ urlpatterns = patterns(
     url(r'^logout/?$', 'mybudget.views.logout_view', name='logout'),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', DashboardView.as_view(), name='start'),
-    url(r'^dashboard/$', DashboardView.as_view(), name='dashboard'),
-    url(r'^expenses/latest/$', LatestExpenseListView.as_view(), name='latest_expense_list'),
-    url(r'^expenses/$', FilteredExpenseListView.as_view(), name='expense_list'),
-    url(r'^expense/add/$', ExpenseCreateView.as_view(), name='expense_add'),
-    url(r'^expense/(?P<pk>[0-9]+)/$', ExpenseUpdateView.as_view(), name='expense_update'),
-    url(r'^expense/(?P<pk>[0-9]+)/details/$', ExpenseUpdateView.as_view(), name='expense_details'),
-    url(r'^expense/(?P<pk>[0-9]+)/delete/$', ExpenseDeleteView.as_view(), name='expense_delete'),
+    url(r'^$', views.DashboardView.as_view(), name='start'),
+    url(r'^dashboard/$', views.DashboardView.as_view(), name='dashboard'),
+    url(r'^expenses/latest/$', views.LatestExpenseListView.as_view(), name='latest_expense_list'),
+    url(r'^expenses/$', views.FilteredExpenseListView.as_view(), name='expense_list'),
+    url(r'^expense/add/$', views.ExpenseCreateView.as_view(), name='expense_add'),
+    url(r'^expense/(?P<pk>[0-9]+)/$', views.ExpenseUpdateView.as_view(), name='expense_update'),
+    url(r'^expense/(?P<pk>[0-9]+)/details/$', views.ExpenseUpdateView.as_view(), name='expense_details'),
+    url(r'^expense/(?P<pk>[0-9]+)/delete/$', views.ExpenseDeleteView.as_view(), name='expense_delete'),
 
-    url(r'^categories/$', CategoryListView.as_view(), name='category_list'),
-    url(r'^categories/add/$', CategoryCreateView.as_view(), name='category_add'),
-    url(r'^category/(?P<pk>[0-9]+)/$', CategoryUpdateView.as_view(), name='category_update'),
-    url(r'^category/(?P<pk>[0-9]+)/details/$', CategoryUpdateView.as_view(), name='category_details'),
-    url(r'^category/(?P<pk>[0-9]+)/delete/$', CategoryDeleteView.as_view(), name='category_delete'),
+    url(r'^categories/$', views.CategoryListView.as_view(), name='category_list'),
+    url(r'^categories/add/$', views.CategoryCreateView.as_view(), name='category_add'),
+    url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryUpdateView.as_view(), name='category_update'),
+    url(r'^category/(?P<pk>[0-9]+)/details/$', views.CategoryUpdateView.as_view(), name='category_details'),
+    url(r'^category/(?P<pk>[0-9]+)/delete/$', views.CategoryDeleteView.as_view(), name='category_delete'),
 )
