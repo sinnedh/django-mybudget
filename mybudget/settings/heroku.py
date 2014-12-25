4b1630ff742b4259770d68303fcff03e4f507d13
Dennis Hellmich <dennish@gmx.de>
2014-12-25 15:30:33 +0100

    test

--- mybudget/settings/heroku.py
+++ mybudget/settings/heroku.py
@@ -1,6 +1,7 @@
 from mybudget.settings.base import *
 
-BOWER_COMPONENTS_ROOT = BASE_DIR
+#BOWER_COMPONENTS_ROOT = BASE_DIR
+BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, '../components/')
 
 #DATABASE_URL: postgres://mtcoxkptamowqx:qAj3Woq0NlSCqjjADFKdwC2taV@ec2-54-163-255-191.compute-1.amazonaws.com:5432/dcpu7ln0efr0uj
 import dj_database_url
