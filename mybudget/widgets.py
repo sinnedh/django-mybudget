fb0bbd9da1c406fcf7a81ba364f9333c9a112363
Dennis Hellmich <dennish@gmx.de>
2014-12-25 14:04:49 +0100

    fixed translation issue

--- mybudget/widgets.py
+++ mybudget/widgets.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.utils.encoding import force_text
-from django.utils.safestring import mark_safe
 from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy as _
 
 
 ICONS_CHOICES = (
