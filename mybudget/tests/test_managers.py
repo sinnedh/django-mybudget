16ec0088b00a11961be0bb4e387192b5da8170f9
Dennis Hellmich <dennish@gmx.de>
2014-12-29 16:23:35 +0100

    Adapted tests

--- mybudget/tests/test_managers.py
+++ mybudget/tests/test_managers.py
@@ -48,12 +48,18 @@ class ExpenseForOrganisationManagerTests(BaseTests):
 
         categories = Expense.objects.summed_by_category(organisation=self.o1)
         self.assertEqual(len(categories), 2)
-        self.assertEqual(categories[0], {'category_id': c1.id, 'amount_sum': Decimal('131.79')})
-        self.assertEqual(categories[1], {'category_id': c2.id, 'amount_sum': Decimal('78.79')})
+        self.assertEqual(categories[0], {'category_id': c1.id,
+                                         'category__name': 'c1',
+                                         'amount_sum': Decimal('131.79')})
+        self.assertEqual(categories[1], {'category_id': c2.id,
+                                         'category__name': 'c2',
+                                         'amount_sum': Decimal('78.79')})
 
         categories = Expense.objects.summed_by_category(organisation=self.o2)
         self.assertEqual(len(categories), 1)
-        self.assertEqual(categories[0], {'category_id': c3.id, 'amount_sum': Decimal('44.23')})
+        self.assertEqual(categories[0], {'category_id': c3.id,
+                                         'category__name': 'c3',
+                                         'amount_sum': Decimal('44.23')})
 
     def _today_minus_n(self, n=0):
         return datetime.date.today() - datetime.timedelta(days=n)
@@ -98,23 +104,25 @@ class ExpenseForOrganisationManagerTests(BaseTests):
 
         expenses = Expense.objects.get_latest(organisation=self.o1, days=1)
         self.assertItemsEqual(expenses, [e1a])
-        expenses = Expense.objects.get_latest(organisation=self.o1, days=3)
+        expenses = Expense.objects.get_latest(organisation=self.o1, days=2)
         self.assertItemsEqual(expenses, [e1a])
-        expenses = Expense.objects.get_latest(organisation=self.o1, days=4)
+        expenses = Expense.objects.get_latest(organisation=self.o1, days=3)
         self.assertItemsEqual(expenses, [e1a, e1b])
-        expenses = Expense.objects.get_latest(organisation=self.o1, days=5)
+        expenses = Expense.objects.get_latest(organisation=self.o1, days=4)
         self.assertItemsEqual(expenses, [e1a, e1b])
         expenses = Expense.objects.get_latest(organisation=self.o1, days=30)
         self.assertItemsEqual(expenses, [e1a, e1b, e1c])
 
-        expenses = Expense.objects.get_latest(organisation=self.o2, days=2)
+        expenses = Expense.objects.get_latest(organisation=self.o2, days=1)
         self.assertItemsEqual(expenses, [])
+        expenses = Expense.objects.get_latest(organisation=self.o2, days=2)
+        self.assertItemsEqual(expenses, [e2a])
         expenses = Expense.objects.get_latest(organisation=self.o2, days=6)
         self.assertItemsEqual(expenses, [e2a, e2b])
 
     def test_get_latest_with_account(self):
         c1 = Category.objects.create(name='c1', organisation=self.o1)
-        c2 = Category.objects.create(name='c2', organisation=self.o2)
+        c2 = Category.objects.create(name='c2', organisation=self.o1)
 
         e1a = Expense.objects.create(category=c1, account=self.a1, amount=34.56,
                                      date=self._today_minus_n(0))
@@ -124,23 +132,25 @@ class ExpenseForOrganisationManagerTests(BaseTests):
                                      date=self._today_minus_n(21))
 
         e2a = Expense.objects.create(category=c2, account=self.a2, amount=34.56,
-                                     date=self._today_minus_n(2))
+                                     date=self._today_minus_n(3))
         e2b = Expense.objects.create(category=c2, account=self.a2, amount=44.23,
                                      date=self._today_minus_n(5))
 
         expenses = Expense.objects.get_latest(organisation=self.o1, account=self.a1, days=1)
         self.assertItemsEqual(expenses, [e1a])
-        expenses = Expense.objects.get_latest(organisation=self.o1, account=self.a1, days=3)
+        expenses = Expense.objects.get_latest(organisation=self.o1, account=self.a1, days=2)
         self.assertItemsEqual(expenses, [e1a])
-        expenses = Expense.objects.get_latest(organisation=self.o1, account=self.a1, days=4)
+        expenses = Expense.objects.get_latest(organisation=self.o1, account=self.a1, days=3)
         self.assertItemsEqual(expenses, [e1a, e1b])
-        expenses = Expense.objects.get_latest(organisation=self.o1, account=self.a1, days=5)
+        expenses = Expense.objects.get_latest(organisation=self.o1, account=self.a1, days=4)
         self.assertItemsEqual(expenses, [e1a, e1b])
         expenses = Expense.objects.get_latest(organisation=self.o1, account=self.a1, days=30)
         self.assertItemsEqual(expenses, [e1a, e1b, e1c])
 
         expenses = Expense.objects.get_latest(organisation=self.o1, account=self.a2, days=2)
         self.assertItemsEqual(expenses, [])
+        expenses = Expense.objects.get_latest(organisation=self.o1, account=self.a2, days=3)
+        self.assertItemsEqual(expenses, [e2a])
         expenses = Expense.objects.get_latest(organisation=self.o1, account=self.a2, days=6)
         self.assertItemsEqual(expenses, [e2a, e2b])
 
